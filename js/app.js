91b4cdfa7d65ebfe37467732a98fdff619b44f5d
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-16 09:44:00 -1000

    current state

--- js/app.js
+++ js/app.js
@@ -1,50 +1,91 @@
-let subRedArr = ["https://www.reddit.com/r/comics/.json?raw_json=1", "https://www.reddit.com/r/ramen/.json?raw_json=1", "https://www.reddit.com/r/OldSchoolCool/.json?raw_json=1"];
+//Request Function:
+const request = (url, callback) => {
+  let oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", callback);
+  oReq.open("GET", url);
+  oReq.send();
+};
 
-const mainContainer = document.getElementById('container')
-rebirthReddit(); //Default page load
+let randomClicked = false;
+let myBoardsClicked = false;
+let getTheAppClicked = false;
+let url = null;
+let postsClass = document.getElementsByClassName("posts");
 
-let subRedLink = document.getElementsByClassName("menu-items");
-subRedLink[0].addEventListener("click", () => {rebirthReddit(subRedArr[0])});
-subRedLink[1].addEventListener("click", () => {rebirthReddit(subRedArr[1])});
-subRedLink[2].addEventListener("click", () => {rebirthReddit(subRedArr[2])});
+//Initial population of page
+populate("https://www.reddit.com/r/toyotatacoma.json");
 
+//If a nav button is clicked, change the request url
+document.getElementById("random").addEventListener("click", setRandomClicked);
+function setRandomClicked() {
+  console.log("random clicked");
+  populate("https://www.reddit.com/r/nissangtr.json");
+};
 
-function rebirthReddit(subRed) {
+document.getElementById("myBoards").addEventListener("click", setMyBoardsClicked);
+function setMyBoardsClicked() {
+  console.log("myBoards clicked");
+  populate("https://www.reddit.com/r/toyotatacoma.json");
+  console.log('OHMYGAWD', populate);
+};
 
-  let redObj = {} 
-  let cardsNum = 0; 
-  let reqRed = new XMLHttpRequest();
-  reqRed.addEventListener("load", getSubReddit);
+document.getElementById("getTheApp").addEventListener("click", setGetTheAppClicked);
+function setGetTheAppClicked() {
+  console.log("getTheApp clicked");
+  populate("https://www.reddit.com/r/money.json");
+};
 
-  if(subRed) {
-    reqRed.open("GET", subRed);
-  } else {
-    reqRed.open("GET", "https://www.reddit.com/r/toyotatacoma/.json"); 
+//Function to populate the reddit page with posts
+function populate(url) {
+//extract data, then new function to the get data from url.
+  let postsBodyElem = document.getElementById("postsBody");
+
+  //Remove all the previous posts so the new url's posts can be populated
+  for (let j = postsClass.length - 1; j >= 0; j--) {
+    postsClass[j].remove();
+    //postsBodyElem.removeChild(postsBodyElem.childNodes[j])
   }
 
-  reqRed.send();
+  //Populate the page with posts
+  request(url, res => {
+    let myObj = JSON.parse(res.currentTarget.response);
+  
+    let childrenData = myObj.data.children[i].data
+    // for (let i = 0; i < myObj.data.children.length; i++) {
+    //   // look back at fun arrays. /1st
+    //   //Create divs
+    //   let postElem = document.createElement("div");
+    //   postElem.className = "posts";
+    //   let titleElem = document.createElement("div");
+    //   titleElem.id = "title";
+    //   let authorElem = document.createElement("div");
+    //   authorElem.id = "author";
 
-    function getSubReddit () {
-      redObj = JSON.parse(this.responseText);
-      console.log('proper', redObj);
-      const title = document.createElement('div');
-      redObj.data.children.forEach(element => {
-        console.log('key', element.data.title);    
-        title.innerHTML = element.data.title;
-      });;
-      mainContainer.append(title)
-      makeCards(cardsNum);
-    
-    }
+    //   //Get data from url //function 
+    //   console.log("response: ", myObj);
 
-  // function makeCards (count) {
-  //   let cardsParent = document.getElementById("cardParent");
-  //   cardsParent.innerHTML = (cardsParent); // Wipes cards-parent clean
+    //   console.log("post title: ", myObj.data.children[i].data.title);
+    //   let postTitle = myObj.data.children[i].data.title;
 
-  //   for (let i = 0; i < count; i++) {
-  //     let cards = document.createElement("div");
-  //     cards.className = "cards";
+    //   console.log("post author: ", myObj.data.children[i].data.author);
+    //   let username = myObj.data.children[i].data.author;
 
-  //   }
-  // }
+    //   console.log("post ups: ", myObj.data.children[i].data.upsCount);
+    //   let postUps = myObj.data.children[i].data.ups;
+
+    //   console.log("post comment count: ", myObj.data.children[i].data.num_comments);
+    //   let numComments = myObj.data.children[i].data.num_comments;
+
+    //   console.log("media link: ", myObj.data.children[i].data.url);
+    //   let mediaLink = myObj.data.children[i].data.url;
+    //   console.log("domain: ", mediaLink.slice(10, 14))
+    //   let domainStr = myObj.data.children[i].data.domain;
+    //   console.log("thumbnail: ", myObj.data.children[i].data.thumbnail)
+    //   let thumbnailLink = myObj.data.children[i].data.thumbnail;
+
+    //   //Put info into html file
+    //   // if (myObj.data.children[i].data.post_hint === "image") {
+    //   // }
+    // }
+  })
 }
