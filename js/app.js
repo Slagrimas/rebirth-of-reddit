fdab72224687d594ec135922f26a81c33931916d
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-16 15:32:50 -1000

    going backwards

--- js/app.js
+++ js/app.js
@@ -55,77 +55,71 @@ function populate(url) {
 
     function cleanData (post) {
       const newPost = {};
-      newPost.title = post.data.title;
+    newPost.title = post.data.title;
       newPost.author = post.data.author;
       return newPost;
     }
 
-    console.log('keyy', scrubData);
     var cardsElems = scrubData.map(cardData => {
-      console.log('carrdddsss', cardData); //pretty much newPost
-      //createCard(cardData) 
-    });
-    function createCard (cardData) {
       //dom creation
-      const cardsDisplay = document.createElement('div'); 
-      
-    }
-    //append cardsElems here
-    // let postElem = document.createElement("div");
-    // postElem.className = "posts";
-    // let titleElem = document.createElement("div");
-    // titleElem.id = "title";
-    // let authorElem = document.createElement("div");
-    // authorElem.id = "author";
-     
-    //   //Get data from url //function 
-    //   console.log("response: ", myObj);
+      let postElem = document.createElement("div");
+      postElem.className = "posts";
+      let titleElem = document.createElement("div");
+      titleElem.id = "title";
+      let authorElem = document.createElement("div");
+      authorElem.id = "author";
+      let cardContainer = document.createElement('div');
+      cardContainer.id = "CardC"
 
-    //   // console.log("post title: ", myObj.data.children[i].data.title);
-    //   let postTitle =breakDown[i].data.title;
-     
-    //   // console.log("post author: ", myObj.data.children[i].data.author);
-    //   let username = breakDown[i].data.author;
+      
+    })
+
+    // console.log('keyy', scrubData);
+    // var cardsElems = scrubData.map(cardData => {
+    //   console.log('GGG', cardData);
+    // });
+
     
-    //   // console.log("post ups: ", myObj.data.children[i].data.upsCount);
-    //   let postUps = breakDown[i].data.ups;
 
-    //   // console.log("post comment count: ", myObj.data.children[i].data.num_comments);
-    //   let numComments = breakDown[i].data.num_comments;
 
-    //   // console.log("media link: ", myObj.data.children[i].data.url);
-    //   let mediaLink = breakDown[i].data.url;
-    //   // console.log("domain: ", mediaLink.slice(10, 14))
-    //   let domainStr = breakDown[i].data.domain;
-    //   // console.log("thumbnail: ", myObj.data.children[i].data.thumbnail)
-    //   let thumbnailLink = breakDown.data.thumbnail;
-
-    //   //Put info into html file
-    //   if (breakDown[i].data.post_hint === "image") {
-    //     let imageElem = document.createElement("img");
-    //     if (domainStr === "i.redd.it") {
-    //       imageElem.src = thumbnailLink;
-    //       postElem.appendChild(imageElem);
-    //     }
-    //     else {
-    //       imageElem.src = mediaLink;
-    //       postElem.appendChild(imageElem);
-    //     }
+    //   function buildCard (cardData) {
+    //     console.log(cardData);
+    //    const buildC = {}
+    //    const buildsCards = cardData
+    //    return buildsCards;
     //   }
-    //   else if (breakDown[i].data.post_hint === "link") {
-    //     let imageElem = document.createElement("img");
-      
-    //    //If this is a gif and it has a 'v' at the end of the link, remove the v for the gif to load properly on the page
-    //    if (mediaLink[mediaLink.length - 1] === "v") {
-    //     let gifLink = mediaLink.slice(0, length - 1);
-    //     console.log("gifLink: ", gifLink);
-    //     imageElem.src = gifLink;
-    //     postElem.appendChild(imageElem);
-    //   }
-    //   else {
+    //  console.log('kabam', buildCard())
+      //Get data
+      // console.log("response: ", myObj);
+
+      // let postTitle = myObj.data.children[i].data.title;
+
+      // let username = myObj.data.children[i].data.author;
+
+      // let postUps = myObj.data.children[i].data.ups;
+
+      // let numComments = myObj.data.children[i].data.num_comments;
+
+      // let mediaLink = myObj.data.children[i].data.url;
+
+      // let domainStr = myObj.data.children[i].data.domain;
+
+      // let thumbnailLink = myObj.data.children[i].data.thumbnail;
+
+     //Put info into html file
+    //  if (myObj.data.children[i].data.post_hint === "image") {
+    //   let imageElem = document.createElement("img");
+    //   if (domainStr === "i.redd.it") {
     //     imageElem.src = thumbnailLink;
     //     postElem.appendChild(imageElem);
     //   }
+    //   else {
+    //     imageElem.src = mediaLink;
+    //     postElem.appendChild(imageElem);
+    //   }
+    // }
+    // else if (myObj.data.children[i].data.post_hint === "link") {
+    //   let imageElem = document.createElement("img");
     // }
   })
 }
