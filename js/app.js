4f25fd774c0a9acd8d40d5faf87afc6218cbe387
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-16 20:10:06 -1000

    ayyyyyy

--- js/app.js
+++ js/app.js
@@ -44,98 +44,63 @@ function populate(url) {
     postsClass[j].remove();
     //postsBodyElem.removeChild(postsBodyElem.childNodes[j])
   }
-
+  const postBody = document.querySelector('#postsBody');
   //Populate the page with posts
   request(url, res => {
     let myObj = JSON.parse(res.currentTarget.response);
-    
-    for (let i = 0; i < myObj.data.children.length; i++) {
-      //Create divs
+
+    const breakDown = myObj.data.children;
+    const scrubData = breakDown.map(cleanData);
+
+    function cleanData (post) {
+      const newPost = {};
+    newPost.title = post.data.title;
+      newPost.author = post.data.author;
+      newPost.selftext = post.data.selftext;
+      newPost.date = post.data.date;
+      newPost.thumbnail = post.data.thumbnail;
+      return newPost;
+    }
+
+    var cardsElems = scrubData.map(cardData => {
+      console.log('LOLOLOL', cardData)
+      //dom creation
       let postElem = document.createElement("div");
-      postElem.className = "posts";
+      console.log(cardData.title);
+      postElem.id = "posts";
+      postElem.innerHTML = " ";
+
       let titleElem = document.createElement("div");
       titleElem.id = "title";
+      titleElem.innerHTML = cardData.title
+
       let authorElem = document.createElement("div");
       authorElem.id = "author";
+      authorElem.innerHTML = cardData.author
 
-      //Get data from url
-      console.log("response: ", myObj);
+      let selftextElem = document.createElement("div");
+      selftextElem.id = "selftext"
+     selftextElem.innerHTML = cardData.selftext
 
-      console.log("post title: ", myObj.data.children[i].data.title);
-      let postTitle = myObj.data.children[i].data.title;
+     let thumbnailElem = document.createElement("img")
+     thumbnailElem.id = "thumbNail"
+    //  thumbnailElem.innerHTML = cardData.data.thumbnail;
+     thumbnailElem.src = cardData.thumbnail
+    //  console.log("yeahUHUH", cardData.thumbnail);
 
-      console.log("post author: ", myObj.data.children[i].data.author);
-      let username = myObj.data.children[i].data.author;
+     let dateElem = document.createElement("div");
+     dateElem.id = "dateElem"
+     dateElem.innerHTML = "10/17/18" 
 
-      console.log("post ups: ", myObj.data.children[i].data.upsCount);
-      let postUps = myObj.data.children[i].data.ups;
-
-      console.log("post comment count: ", myObj.data.children[i].data.num_comments);
-      let numComments = myObj.data.children[i].data.num_comments;
-
-      console.log("media link: ", myObj.data.children[i].data.url);
-      let mediaLink = myObj.data.children[i].data.url;
-      console.log("domain: ", mediaLink.slice(10, 14))
-      let domainStr = myObj.data.children[i].data.domain;
-      console.log("thumbnail: ", myObj.data.children[i].data.thumbnail)
-      let thumbnailLink = myObj.data.children[i].data.thumbnail;
-
-      //Put info into html file
-      if (myObj.data.children[i].data.post_hint === "image") {
-        let imageElem = document.createElement("img");
-        if (domainStr === "i.redd.it") {
-          imageElem.src = thumbnailLink;
-          postElem.appendChild(imageElem);
-        }
-        else {
-          imageElem.src = mediaLink;
-          postElem.appendChild(imageElem);
-        }
-      }
-      else if (myObj.data.children[i].data.post_hint === "link") {
-        let imageElem = document.createElement("img");
-
-        //If this is a gif and it has a 'v' at the end of the link, remove the v for the gif to load properly on the page
-        if (mediaLink[mediaLink.length - 1] === "v") {
-          let gifLink = mediaLink.slice(0, length - 1);
-          console.log("gifLink: ", gifLink);
-          imageElem.src = gifLink;
-          postElem.appendChild(imageElem);
-        }
-        else {
-          imageElem.src = thumbnailLink;
-          postElem.appendChild(imageElem);
-        }
-
-      }
-
-      titleElem.innerHTML = postTitle;
       postElem.appendChild(titleElem);
-
-      authorElem.innerHTML = "by " + username + " • Ups: " + postUps + " • Comments: " + numComments;
-      postElem.appendChild(authorElem);
-
-      postsBodyElem.appendChild(postElem);
-    }
+      titleElem.appendChild(authorElem);
+      authorElem.appendChild(selftextElem)
+      selftextElem.appendChild(dateElem)
+      dateElem.appendChild(thumbnailElem)
+      // const postBody = document.querySelector('postsBody');
+      postsBody.appendChild(postElem);
+      
+    })
+    console.log('UHUHUH', cardsElems);
   })
 }
-    // const breakDown = myObj.data.children;
-    // const scrubData = breakDown.map(cleanData);
-
-    // function cleanData (post) {
-    //   const newPost = {};
-    // newPost.title = post.data.title;
-    //   newPost.author = post.data.author;
-    //   return newPost;
-    // }
-
-    // var cardsElems = scrubData.map(cardData => {
-    //   //dom creation
-    //   let postElem = document.createElement("div");
-    //   postElem.className = "posts";
-    //   let titleElem = document.createElement("div");
-    //   titleElem.id = "title";
-    //   let authorElem = document.createElement("div");
-    //   authorElem.id = "author";
-    //   let cardContainer = document.createElement('div');
-    //   cardContainer.id = "CardC"
